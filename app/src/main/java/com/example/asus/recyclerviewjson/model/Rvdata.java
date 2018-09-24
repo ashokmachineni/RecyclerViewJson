3abbc0dc7eb7a8089004cb1f8cbd0cbe441fdc31
ashokmachineni <ashok5679m@gmail.com>
2018-09-25 01:00:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/asus/recyclerviewjson/model/Rvdata.java
@@ -0,0 +1,26 @@
+package com.example.asus.recyclerviewjson.model;
+
+public class Rvdata {
+
+    public String title;
+    public String thumbnail;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+
+}
